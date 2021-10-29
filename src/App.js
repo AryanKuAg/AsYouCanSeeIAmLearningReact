06c5da7891d27a3cb74ca7582d49e5bb578b669a
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-10-29 20:56:10 +0530

    dekho

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Card from './Card/Card'
+
+
 function App(){
-    return <h1>Hey its aryan and this is my website</h1>
+    const buttonHandler = () => {
+        console.log('press the button')
+    }
+    
+    return <div>
+        <h1>Hey its aryan and this is my website</h1>
+        <Card mydata="kkkkkkkkkkkkkkkkkkkkkkkkkk" thebutton={buttonHandler}/>
+    </div>
 }
 
 export default App
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,20 @@
-function Card(){
-    return <p>Nothing to say</p>
+import {useState} from 'react'
+
+
+function Card(props) {
+    const [buttonName, setButtonName] = useState('Press')
+ 
+    
+  const dummyData = {
+    name: "aryan",
+    age: 10,
+    sex: "Male",
+  };
+  return (
+    <button type="button" class="btn btn-primary" onClick={() => {setButtonName('Aryan')}}>
+      {buttonName}
+    </button>
+  );
 }
 
-export default Card
+export default Card;
